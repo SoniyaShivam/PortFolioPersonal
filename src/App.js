1905f62d5acb6a8b3daed789c0594055f4fef015
Soniya Sadasivam <148883264+SoniyaShivam@users.noreply.github.com>
2024-08-20 15:05:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+function App() {
+return (
+<div className="App">
+<Header />
+<Home />
+</div>
+);
+}
+export default App;
